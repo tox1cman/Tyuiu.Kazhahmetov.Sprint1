38a8f3158deb76aa5de8e37d270f2dbeb2dd189b
Таджикович Ержан <kazamircik@gmail.com>
2025-09-21 16:37:34 +0500

    Task6

---
+++ Tyuiu.Kazhahmetov.Sprint1.Task6.V15.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+namespace Tyuiu.Kazhahmetov.Sprint1.Task6.V15.Lib
+{
+    public class DataService : ISprint1Task6V15
+    {
+        public bool CheckLettersCount(string value)
+        {
+            
+        }
+    }
+}
---
+++ Tyuiu.Kazhahmetov.Sprint1.Task6.V15.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.Kazhahmetov.Sprint1.Task6.V15.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
